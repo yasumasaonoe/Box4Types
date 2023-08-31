aea54256f51c36dc3f9f5323a73d6af376d71553
Yasumasa Onoe <yasumasa@cs.utexas.edu>
2023-08-31 12:55:03 -0500

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1erPs4WlH9Hma5oU03BwsIkBa_DvZL73e' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1erPs4WlH9Hma5oU03BwsIkBa_DvZL73e" -O data.tar.gz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/file/d/1DxhjtfMoDulK8HIduGVOhYrJjvdUTLSb/view?usp=sharing' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1DxhjtfMoDulK8HIduGVOhYrJjvdUTLSb" -O data.tar.gz && rm -rf /tmp/cookies.txt
 tar -xf data.tar.gz
 rm data.tar.gz
